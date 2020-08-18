4944b1534acd4c7dd8c6caa99d721c25e330cd05
Nic Pell <nicole.pellicena@triptease.com>
2020-08-18 08:41:02 +0100

    setting and using env variables, using plugins in nextjs config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
+const nextEnv = require('next-env')
+const dotenvLoad = require('dotenv-load')
+
 const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+
+dotenvLoad()
+
+const withNextEnv = nextEnv()
 
 module.exports = (phase, { defaultConfig }) => {
   if (phase === PHASE_DEVELOPMENT_SERVER) {
     console.log("I'm in dev mode!")
 
-    return defaultConfig
+    return withNextEnv(defaultConfig)
   }
-  return defaultConfig
+  return withNextEnv(defaultConfig)
 }
